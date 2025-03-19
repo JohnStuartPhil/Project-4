8998287dad513e985f6384a2717fa431a5c08a4d
JohnStuartPhil <0q5av15l@students.codeinstitute.net>
2025-03-19 20:43:45 +0000

    Heroku test

--- add_bands/views.py
+++ add_bands/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def my_bands(request):
-    return HttpResponse("Hello project 4")
+    return HttpResponse("Hello project 4 for Heroku")
