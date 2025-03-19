49e233de770df762c668727cebc33dde4fc2797a
JohnStuartPhil <0q5av15l@students.codeinstitute.net>
2025-03-19 20:20:14 +0000

    Initial test for Heroku

--- band_blog/settings.py
+++ band_blog/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xik7(17ro82#6js2vm-_v7k4_dv*5v18by1254ke!)+2*xss(h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
