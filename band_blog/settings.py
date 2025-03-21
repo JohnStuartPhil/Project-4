d0ed8e57746b9ba3abbe00f25b298c5ffb00f20d
JohnStuartPhil <0q5av15l@students.codeinstitute.net>
2025-03-21 16:56:38 +0000

    Add secret key

--- band_blog/settings.py
+++ band_blog/settings.py
@@ -24,10 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xik7(17ro82#6js2vm-_v7k4_dv*5v18by1254ke!)+2*xss(h'
+# SECRET_KEY = 'django-insecure-xik7(17ro82#6js2vm-_v7k4_dv*5v18by1254ke!)+2*xss(h'
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
