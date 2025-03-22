f642d753fad8ee4c196b213b0e0a264a10b0c7ea
JohnStuartPhil <0q5av15l@students.codeinstitute.net>
2025-03-22 15:53:38 +0000

    Add post model

--- add_bands/models.py
+++ add_bands/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator
+from django.core.validators import MaxValueValidator
+
+STATUS = ((0, "Draft"), (1, "Published"))
+GENRE_CHOICES = (('genre', 'GENRE'), ('pop', 'POP'),('rock', 'ROCK'),('blues', 'BLUES'),('jazz', 'JAZZ'),('other', 'OTHER'),)
 
 # Create your models here.
+class Post(models.Model):
+    name_of_band = models.CharField(max_length=25, unique=True)
+    genre = models.CharField(max_length=6, choices=GENRE_CHOICES, default='genre')
+    number_of_members = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(7)], unique=False)
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)], unique=False)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="band_posts")
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
--- env.py
+++ env.py
@@ -1,6 +1,5 @@
 import os
 
-os.environ.setdefault(
-    "DATABASE_URL", "postgresql://neondb_owner:npg_H8eMNcSE2bqg@ep-black-frog-a21mffr1.eu-central-1.aws.neon.tech/pupil_blast_moist_424218")
+os.environ.setdefault("DATABASE_URL", "postgresql://neondb_owner:npg_H8eMNcSE2bqg@ep-black-frog-a21mffr1.eu-central-1.aws.neon.tech/pupil_blast_moist_424218")
 
 os.environ.setdefault("SECRET_KEY", "qvyggb4*)(x_)_d)wwznsu@d@xrzvwt2)!#w44%7ejv70a6mgv")
