479127c4deec43ecb763c1e26cded76adadda76e
JohnStuartPhil <0q5av15l@students.codeinstitute.net>
2025-03-23 16:41:37 +0000

    chnage name from thoughts on this band to band review

--- add_bands/models.py
+++ add_bands/models.py
@@ -12,7 +12,7 @@ class Post(models.Model):
     genre = models.CharField(max_length=6, choices=GENRE_CHOICES, default='genre', unique=False)
     number_of_members = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(7)], unique=False)
     marks_out_of_five = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)], unique=True)
-    your_thoughts_on_this_band = models.TextField(unique=True)
+    band_review = models.TextField(unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="band_posts")
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
