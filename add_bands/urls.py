1e3214e4282915ecc8fdcb87f3729d3c087de6ec
JohnStuartPhil <0q5av15l@students.codeinstitute.net>
2025-03-25 21:43:45 +0000

    Add list view

--- add_bands/urls.py
+++ add_bands/urls.py
@@ -5,5 +5,5 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home'),
+    path('', views.BandPostList.as_view(), name='home'),
 ]
--- add_bands/views.py
+++ add_bands/views.py
@@ -6,6 +6,8 @@ from .models import Band_Post
 # Create your views here.
 #def my_bands(request):
     #return HttpResponse("Hello project 4 for Heroku")
-class PostList(generic.ListView):
-    model = Band_Post
+class BandPostList(generic.ListView):
+    #model = Band_Post
+    queryset = Band_Post.objects.filter(status=1)
+    template_name = "band_post_list.html"
 
