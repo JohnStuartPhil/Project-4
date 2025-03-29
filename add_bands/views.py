378eb5642b435bdc4e9f5bcd128d9e38177f21fe
JohnStuartPhil <0q5av15l@students.codeinstitute.net>
2025-03-29 20:52:44 +0000

    Add text to band_post_list.html

--- add_bands/views.py
+++ add_bands/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 #from django.http import HttpResponse
 from django.views import generic
 from .models import Band_Post
+#from .models import Opinion
 
 # Create your views here.
 #def my_bands(request):
@@ -11,3 +12,7 @@ class BandPostList(generic.ListView):
     queryset = Band_Post.objects.filter(status=1)
     template_name = "band_post_list.html"
 
+#class OpionionList(generic.ListView):
+    #model = Opinion
+    #queryset = Opinion.objects.filter(status=1)
+    #template_name = "band_post_list.html"
