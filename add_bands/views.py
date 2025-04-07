a15b925f3b28bbe97f22a4fef0780911fc8b0128
JohnStuartPhil <0q5av15l@students.codeinstitute.net>
2025-04-07 21:20:55 +0100

    Add authentication

--- add_bands/views.py
+++ add_bands/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 #from django.http import HttpResponse
 from django.views import generic
 from .models import BandPost
-#from .models import Opinion
+from .models import Opinion
 
 # Create your views here.
 #def my_bands(request):
@@ -10,12 +10,12 @@ from .models import BandPost
 class BandPostList(generic.ListView):
     queryset = BandPost.objects.filter(status=1)
     template_name = "add_bands/index.html"
-    paginate_by = 6
+    #paginate_by = 6
     #model = Band_Post
     #template_name = "bandpost_list.html"
 
-#class OpionionList(generic.ListView):
-    #model = Opinion
+class OpionionList(generic.ListView):
+    model = Opinion
     #queryset = Opinion.objects.filter(status=1)
-    #template_name = "bandpost_list.html"
+    template_name = "add_bands/index.html"
 
